5db8cc23de1ec4d123bdc8032c48c649b8d10e0c
rohithreddykota <rohithreddykota@gmail.com>
2023-11-29 17:56:15 -0500

    update ddl and dml dates

--- implementation/database/dml.sql
+++ implementation/database/dml.sql
@@ -80,6 +80,19 @@ VALUES
 ('William', 'Davis', 'william.davis@example.com', 'davisPwd', 'Male', '1996-02-08', 'Regular', 'William Davis', 26),
 ('Sophia', 'Clark', 'sophia.clark@example.com', 'passSophia', 'Female', '1991-12-03', 'Regular', 'Sophia Clark', 31);
 
+INSERT INTO [User] (User_FirstName, User_LastName, UserEmail, UserPassword, Gender, DateofBirth, UserType, User_FullName, Age)
+VALUES
+  ('Alifie', 'Johnson', 'alice.johnson@example.com', 'password456', 'Female', '1993-05-20', 'Regular', 'Alice Johnson', 29),
+  ('Bob', 'Anderson', 'bob.anderson@example.com', 'pass123', 'Male', '1990-09-15', 'Regular', 'Bob Anderson', 32),
+  ('Catherine', 'Smith', 'catherine.smith@example.com', 'securePwd789', 'Female', '1987-02-10', 'Regular', 'Catherine Smith', 35),
+  ('David', 'Williams', 'david.williams@example.com', 'p@ssw0rd123', 'Male', '1995-11-25', 'Regular', 'David Williams', 27),
+  ('Eva', 'Jones', 'eva.jones@example.com', 'secret789', 'Female', '1984-07-18', 'Regular', 'Eva Jones', 38),
+  ('Frank', 'Taylor', 'frank.taylor@example.com', 'securePwd456', 'Male', '1998-03-30', 'Regular', 'Frank Taylor', 24),
+  ('Grace', 'Brown', 'grace.brown@example.com', 'brownPass789', 'Female', '1991-12-05', 'Regular', 'Grace Brown', 30),
+  ('Harry', 'Clark', 'harry.clark@example.com', 'passHarry789', 'Male', '1996-06-20', 'Regular', 'Harry Clark', 26),
+  ('Ivy', 'Miller', 'ivy.miller@example.com', 'millerPwd789', 'Female', '1994-03-10', 'Regular', 'Ivy Miller', 28),
+  ('James', 'Davis', 'james.davis@example.com', 'davisPwd789', 'Male', '1992-08-15', 'Regular', 'James Davis', 30);
+
 
 INSERT INTO Seller
   (
@@ -98,7 +111,7 @@ VALUES
   (8, 3, 'Paypal'),
   (9, 2, 'Paypal'),
   (10, 1, 'Paypal');
-
+  
 
 INSERT INTO Buyer
   (
@@ -202,15 +215,15 @@ INSERT INTO Bid
   )
   VALUES 
   (1, 1000.00, '2023-11-10'),
-  (2, 900.00, '2023-11-10'),
-  (3, 800.00, '2023-11-10'),
-  (4, 700.00, '2023-11-10'),
-  (5, 600.00, '2023-11-10'),
-  (6, 500.00, '2023-11-10'),
-  (7, 400.00, '2023-11-10'),
-  (8, 300.00, '2023-11-10'),
-  (9, 200.00, '2023-11-10'),
-  (10, 100.00, '2023-11-10');
+  (2, 900.00, '2023-11-11'),
+  (3, 800.00, '2023-11-12'),
+  (4, 700.00, '2023-11-13'),
+  (5, 600.00, '2023-11-14'),
+  (6, 500.00, '2023-11-15'),
+  (7, 400.00, '2023-11-16'),
+  (8, 300.00, '2023-11-17'),
+  (9, 200.00, '2023-11-18'),
+  (10, 100.00, '2023-11-19');
 
 
 INSERT INTO BidLog
@@ -218,19 +231,60 @@ INSERT INTO BidLog
     BidID,
     BuyerID,
     BidStatusID,
+    BidPrice,
     BidTimestamp
   )
   VALUES 
-  (1, 1, 1, '2023-11-10'),
-  (2, 2, 1, '2023-11-10'),
-  (3, 3, 1, '2023-11-10'),
-  (4, 4, 1, '2023-11-10'),
-  (5, 5, 1, '2023-11-10'),
-  (6, 6, 1, '2023-11-10'),
-  (7, 7, 1, '2023-11-10'),
-  (8, 8, 1, '2023-11-10'),
-  (9, 9, 1, '2023-11-10'),
-  (10, 10, 1, '2023-11-10');
+  (1, 1, 1, 1000.00, '2023-11-10'),
+  (2, 2, 1, 900.00, '2023-11-11'),
+  (3, 3, 1, 800.00, '2023-11-12'),
+  (4, 4, 1, 700.00, '2023-11-13'),
+  (5, 5, 1, 600.00, '2023-11-14'),
+  (6, 6, 1, 500.00, '2023-11-15'),
+  (7, 7, 1, 400.00, '2023-11-16'),
+  (8, 8, 1, 300.00, '2023-11-17'),
+  (9, 9, 1, 200.00, '2023-11-18'),
+  (10, 10, 1, 100.00, '2023-11-19'),
+  (1, 1, 1, 1000.00, '2023-11-20'),
+  (2, 2, 1, 900.00, '2023-11-21'),
+  (3, 3, 1, 800.00, '2023-11-22'),
+  (4, 4, 1, 700.00, '2023-11-23'),
+  (5, 5, 1, 600.00, '2023-11-24'),
+  (6, 6, 1, 500.00, '2023-11-25'),
+  (7, 7, 1, 400.00, '2023-11-26'),
+  (8, 8, 1, 300.00, '2023-11-27'),
+  (9, 9, 1, 200.00, '2023-11-28'),
+  (10, 10, 1, 100.00, '2023-11-29'),
+  (1, 1, 1, 1000.00, '2023-11-30'),
+  (2, 2, 1, 900.00, '2023-12-01'),
+  (3, 3, 1, 800.00, '2023-12-02'),
+  (4, 4, 1, 700.00, '2023-12-03'),
+  (5, 5, 1, 600.00, '2023-12-04'),
+  (6, 6, 1, 500.00, '2023-12-05'),
+  (7, 7, 1, 400.00, '2023-12-06'),
+  (8, 8, 1, 300.00, '2023-12-07'),
+  (9, 9, 1, 200.00, '2023-12-08'),
+  (10, 10, 1, 100.00, '2023-12-09'),
+  (1, 1, 1, 1000.00, '2023-12-10'),
+  (2, 2, 1, 900.00, '2023-12-11'),
+  (3, 3, 1, 800.00, '2023-12-12'),
+  (4, 4, 1, 700.00, '2023-12-13'),
+  (5, 5, 1, 600.00, '2023-12-14'),
+  (6, 6, 1, 500.00, '2023-12-15'),
+  (7, 7, 1, 400.00, '2023-12-16'),
+  (8, 8, 1, 300.00, '2023-12-17'),
+  (9, 9, 1, 200.00, '2023-12-18'),
+  (10, 10, 1, 100.00, '2023-12-19'),
+  (1, 1, 1, 1000.00, '2023-12-20'),
+  (2, 2, 1, 900.00, '2023-12-21'),
+  (3, 3, 1, 800.00, '2023-12-22'),
+  (4, 4, 1, 700.00, '2023-12-23'),
+  (5, 5, 1, 600.00, '2023-12-24'),
+  (6, 6, 1, 500.00, '2023-12-25'),
+  (7, 7, 1, 400.00, '2023-12-26'),
+  (8, 8, 1, 300.00, '2023-12-27'),
+  (9, 9, 1, 200.00, '2023-12-28'),
+  (10, 10, 1, 100.00, '2023-12-29');
 
 
 INSERT INTO WatchList
@@ -242,15 +296,15 @@ INSERT INTO WatchList
   )
   VALUES 
   (1, 'WatchList 1', 'WatchList 1 Description', '2023-11-10'),
-  (2, 'WatchList 2', 'WatchList 2 Description', '2023-11-10'),
-  (3, 'WatchList 3', 'WatchList 3 Description', '2023-11-10'),
-  (4, 'WatchList 4', 'WatchList 4 Description', '2023-11-10'),
-  (5, 'WatchList 5', 'WatchList 5 Description', '2023-11-10'),
-  (6, 'WatchList 6', 'WatchList 6 Description', '2023-11-10'),
-  (7, 'WatchList 7', 'WatchList 7 Description', '2023-11-10'),
-  (8, 'WatchList 8', 'WatchList 8 Description', '2023-11-10'),
-  (9, 'WatchList 9', 'WatchList 9 Description', '2023-11-10'),
-  (10, 'WatchList 10', 'WatchList 10 Description', '2023-11-10');
+  (2, 'WatchList 2', 'WatchList 2 Description', '2023-11-11'),
+  (3, 'WatchList 3', 'WatchList 3 Description', '2023-11-12'),
+  (4, 'WatchList 4', 'WatchList 4 Description', '2023-11-13'),
+  (5, 'WatchList 5', 'WatchList 5 Description', '2023-11-14'),
+  (6, 'WatchList 6', 'WatchList 6 Description', '2023-11-15'),
+  (7, 'WatchList 7', 'WatchList 7 Description', '2023-11-16'),
+  (8, 'WatchList 8', 'WatchList 8 Description', '2023-11-17'),
+  (9, 'WatchList 9', 'WatchList 9 Description', '2023-11-18'),
+  (10, 'WatchList 10', 'WatchList 10 Description', '2023-11-19');
 
 
 INSERT INTO AdItemWatchList
@@ -262,15 +316,15 @@ INSERT INTO AdItemWatchList
   )
   VALUES
   (1, 1, 'Active', '2023-11-10'),
-  (2, 2, 'Active', '2023-11-10'),
-  (3, 3, 'Active', '2023-11-10'),
-  (4, 4, 'Active', '2023-11-10'),
-  (5, 5, 'Active', '2023-11-10'),
-  (6, 6, 'Active', '2023-11-10'),
-  (7, 7, 'Active', '2023-11-10'),
-  (8, 8, 'Active', '2023-11-10'),
-  (9, 9, 'Active', '2023-11-10'),
-  (10, 10, 'Active', '2023-11-10');
+  (2, 2, 'Active', '2023-11-11'),
+  (3, 3, 'Active', '2023-11-12'),
+  (4, 4, 'Active', '2023-11-13'),
+  (5, 5, 'Active', '2023-11-14'),
+  (6, 6, 'Active', '2023-11-15'),
+  (7, 7, 'Active', '2023-11-16'),
+  (8, 8, 'Active', '2023-11-17'),
+  (9, 9, 'Active', '2023-11-18'),
+  (10, 10, 'Active', '2023-11-19');
 
 
 INSERT INTO Alert
@@ -284,15 +338,15 @@ INSERT INTO Alert
 )
 VALUES
 (1, 1, 1, 'Alert 1', '2023-11-10', 'Bid'),
-(2, 2, 2, 'Alert 2', '2023-11-10', 'Bid'),
-(3, 3, 3, 'Alert 3', '2023-11-10', 'Bid'),
-(4, 4, 4, 'Alert 4', '2023-11-10', 'Bid'),
-(5, 5, 5, 'Alert 5', '2023-11-10', 'Bid'),
-(6, 6, 6, 'Alert 6', '2023-11-10', 'Bid'),
-(7, 7, 7, 'Alert 7', '2023-11-10', 'Bid'),
-(8, 8, 8, 'Alert 8', '2023-11-10', 'Bid'),
-(9, 9, 9, 'Alert 9', '2023-11-10', 'Bid'),
-(10, 10, 10, 'Alert 10', '2023-11-10', 'Bid');
+(2, 2, 2, 'Alert 2', '2023-11-11', 'Bid'),
+(3, 3, 3, 'Alert 3', '2023-11-12', 'Bid'),
+(4, 4, 4, 'Alert 4', '2023-11-13', 'Bid'),
+(5, 5, 5, 'Alert 5', '2023-11-14', 'Bid'),
+(6, 6, 6, 'Alert 6', '2023-11-15', 'Bid'),
+(7, 7, 7, 'Alert 7', '2023-11-16', 'Bid'),
+(8, 8, 8, 'Alert 8', '2023-11-17', 'Bid'),
+(9, 9, 9, 'Alert 9', '2023-11-18', 'Bid'),
+(10, 10, 10, 'Alert 10', '2023-11-19', 'Bid');
 
 INSERT INTO Chat
 (
@@ -331,4 +385,3 @@ VALUES
 (4, 8, '2023-11-10', 'Spam'),
 (5, 9, '2023-11-10', 'Spam'),
 (5, 10, '2023-11-10', 'Spam');
-
