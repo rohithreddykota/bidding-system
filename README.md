2b1db6cdf7cbc8c9d75718299a889a9d52d4de48
rohithreddykota <rohithreddykota@gmail.com>
2023-11-29 19:27:34 -0500

    add views

--- README.md
+++ README.md
@@ -15,3 +15,26 @@ Introducing bidding feature to the existing Facebook marketplace.
 [P1. Topic & Objectives: Introduction of Bidding System on Facebook Marketplace](P1.%20Topic%20&%20Objectives.md)
 
 [P2. Database Design, Initial ERD](P2.%20Database%20Design%2C%20Initial%20ERD.md)
+
+[P3. Final ERD](P3.%20Final%20ERD.md)
+
+[P4. Implementation](P4.%20Implementation.md)
+
+### Object Database Design
+
+**Table:**
+
+| Object Type                        | Quantity or Comments | Yes/No |
+| ---------------------------------- | -------------------- | ------ |
+| Tables                             | 9                    |  17      |
+| Views                              | 3                    |        |
+| Table Level Check Constraint       | 4                    |        |
+| Computed Column based on UDF       | 2                    |        |
+| Non-Clustered Indexes              | 4                    |        |
+| Stored Procedures                  | 3                    |        |
+| User Defined Functions (UDF)       | 3                    |        |
+| DML Triggers                       | 1                    |        |
+| Column Data Encryption             | 1                    |        |
+| BI Data Visualization              | Yes                  |        |
+| GUI for CRUD Operations (optional) | No                   |        |
+| Other                              |                      |        |
--- implementation/database/views.sql
+++ implementation/database/views.sql
@@ -1,153 +1,73 @@
--- Stored procedures
-
- use BIDDING_SYSTEM
- -- Stored procedure for Inserting new User
-
-CREATE PROCEDURE InsertNewUser
-    @FirstName VARCHAR(255),
-    @LastName VARCHAR(255),
-    @UserEmail VARCHAR(255),
-    @UserPassword VARCHAR(255),
-    @Gender VARCHAR(255),
-    @DateOfBirth DATE,
-    @UserType VARCHAR(255)
-    
+CREATE VIEW ActiveAuctionsView
 AS
-BEGIN
-    -- Check if the user with the given email already exists
-    IF EXISTS (SELECT 1 FROM [User] WHERE UserEmail = @UserEmail)
-    BEGIN
-        RETURN;
-    END
+  SELECT
+    A.AuctionID,
+    A.Title AS AuctionTitle,
+    A.AuctionDescription,
+    A.BasePrice,
+    A.StartDate,
+    A.EndDate,
+    AI.Title AS AdItemTitle,
+    AI.AdDescription,
+    AI.SellerID,
+    S.SellerRating
+  FROM
+    Auction A
+    JOIN AdItem AI ON A.AdItemID = AI.AdItemID
+    JOIN Seller S ON AI.SellerID = S.SellerID
+  WHERE
+    A.StartDate <= GETDATE() AND A.EndDate >= GETDATE();
 
-    -- Insert the new user
-    INSERT INTO [User] (User_FirstName, User_LastName, UserEmail, UserPassword, Gender, DateofBirth, UserType, User_FullName, Age)
-    VALUES (@FirstName, @LastName, @UserEmail, @UserPassword, @Gender, @DateOfBirth, @UserType, [dbo].GetFullName(@FirstName, @LastName),[dbo].CalculateAge(@DateOfBirth));
 
-END;
- 
-
-EXEC [dbo].InsertNewUser
-    @FirstName = 'John',
-    @LastName = 'Doe',
-    @UserEmail = 'john.doe@example.com',
-    @UserPassword = 'hashed_password',
-    @Gender = 'Male',
-    @DateOfBirth = '1990-01-01',
-    @UserType = 'Regular';
-                                     //-- Stored procedure for Updating a User
-
-  CREATE PROCEDURE UpdateUser
-    @UserID INT,
-    @FirstName VARCHAR(255),
-    @LastName VARCHAR(255),
-    @UserEmail VARCHAR(255),
-    @UserPassword VARCHAR(255),
-    @Gender VARCHAR(255),
-    @DateOfBirth DATE,
-    @UserType VARCHAR(255)
-    
+CREATE VIEW HighlyRatedSellersView
 AS
-BEGIN
-    -- Check if the user with the given email already exists (excluding the current user)
-    IF EXISTS (SELECT 1 FROM [User] WHERE UserEmail = @UserEmail AND UserID <> @UserID)
-    BEGIN
-        RETURN;
-    END
+  SELECT
+    s.SellerID,
+    u.User_FullName AS SellerName,
+    s.SellerRating,
+    s.SellerPaymentInfo
+  FROM
+    Seller s
+    JOIN
+    [User] u ON s.SellerID = u.UserID
+  WHERE
+    s.SellerRating >= 4;
 
-    -- Update the user information
-    UPDATE [User]
-    SET
-        User_FirstName = @FirstName,
-        User_LastName = @LastName,
-        UserEmail = @UserEmail,
-        UserPassword = @UserPassword,
-        Gender = @Gender,
-        DateofBirth = @DateOfBirth,
-        UserType = @UserType,
-        User_FullName = [dbo].GetFullName(@FirstName,@LastName),
-        Age = [dbo].CalculateAge(@DateOfBirth)
-    WHERE UserID = @UserID;
 
-END;
-
--- Stored procedure for Insert AdItem
-
-  CREATE PROCEDURE InsertAdItem
-  @AdminID INT,
-  @CategoryID INT,
-  @SellerID INT,
-  @AdStatusID INT,
-  @Title VARCHAR(255),
-  @AdDescription VARCHAR(255)
+-- Views to display a list of buyers watchlist                                            
+CREATE VIEW BuyerWatchlistView
 AS
-BEGIN
-  SET NOCOUNT ON;
-  INSERT INTO AdItem (AdminID, CategoryID, SellerID, AdStatusID, Title, AdDescription)
-  VALUES (@AdminID, @CategoryID, @SellerID, @AdStatusID, @Title, @AdDescription);
-END;
+  SELECT
+    W.WatchListID,
+    W.BuyerID,
+    W.WatchListName,
+    W.WatchListDescription,
+    AWL.AdItemID,
+    AWL.EventType,
+    AWL.EventTimestamp,
+    AI.Title AS AdItemTitle,
+    AI.AdDescription,
+    AI.CategoryID
+  FROM
+    WatchList W
+    JOIN AdItemWatchList AWL ON W.WatchListID = AWL.WatchListID
+    JOIN AdItem AI ON AWL.AdItemID = AI.AdItemID;
 
-EXEC InsertAdItem
-  @AdminID = 1,            
-  @CategoryID = 2,         
-  @SellerID = 3,           
-  @AdStatusID = 4,         
-  @Title = 'New AdItem',  
-  @AdDescription = 'Description';  
 
-                                  //-- Stored procedure for displaying the buyers list who bought for the same category 
-  CREATE PROCEDURE GetBuyersByCategory
-  @CategoryName VARCHAR(255)
+-- Views to display active users from user
+CREATE VIEW ActiveUsersView
 AS
-BEGIN
-  SET NOCOUNT ON;
-
-  SELECT DISTINCT B.BuyerID, U.User_FullName, U.UserEmail
-  FROM Buyer B
-  INNER JOIN AdItem AI ON B.BuyerID = AI.SellerID
-  INNER JOIN AdItemCategory AC ON AI.CategoryID = AC.CategoryID
-  INNER JOIN [User] U ON B.BuyerID = U.UserID
-  WHERE AC.CategoryName = @CategoryName;
-END;
-
-EXEC GetBuyersByCategory
-  @CategoryName = 'Clothing';
-
--- Stored procedure for deleting a chat
-
-CREATE PROCEDURE DeleteChat
-  @ChatID INT
-AS
-BEGIN
-  SET NOCOUNT ON;
-  DELETE FROM Chat
-  WHERE ChatID = @ChatID;
-  end;
-
-  EXEC DeleteChat
-  @ChatID = 1;
-
-
--- Stored procedure for deleting a chat
-
-  CREATE PROCEDURE DeleteFeedback
-  @FeedbackID INT
-AS
-BEGIN
-  SET NOCOUNT ON;
-  DELETE FROM Feedback
-  WHERE FeedbackID = @FeedbackID;
-END;
-
-EXEC DeleteFeedback
-  @FeedbackID = 3;
-
-                                
-
-                                 
-								   
-
-
-
-
- 	                                                
+  SELECT
+    UserID,
+    User_FirstName,
+    User_LastName,
+    UserEmail,
+    Gender,
+    DateofBirth,
+    UserType,
+    User_FullName,
+    Age
+  FROM
+    [User]
+  WHERE
+    UserType = 'Regular';
